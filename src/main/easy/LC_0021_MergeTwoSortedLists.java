ed25cbb25c77503234ac3a5c0b9800b750736555
Manoj Kumar Singh <manojkumar.singh@livspace.com>
2023-02-08 18:25:09 +0530

    I am getting better everyday

---
+++ src/main/easy/LC_0021_MergeTwoSortedLists.java
@@ -0,0 +1,49 @@
+package main.easy;
+
+import helper.ListNode;
+
+public class LC_0021_MergeTwoSortedLists {
+    //Question : https://leetcode.com/problems/merge-two-sorted-lists/
+    public static void main(String[] args) {
+        new LC_0021_MergeTwoSortedLists().mergeTwoLists(null, null);
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null)
+            return l2;
+
+        if(l2 == null)
+            return l1;
+
+        ListNode h = null;
+        if(l1.val < l2.val){
+            h = l1;
+            l1 = l1.next;
+        }
+        else{
+            h = l2;
+            l2 = l2.next;
+        }
+
+        ListNode t = h;
+        while (l1 != null && l2 != null){
+            if(l1.val < l2.val){
+                t.next = l1;
+                l1 = l1.next;
+            }
+            else{
+                t.next = l2;
+                l2 = l2.next;
+            }
+
+            t = t.next;
+        }
+
+        if(l1 == null)
+            t.next = l2;
+        else t.next = l1;
+
+
+        return h;
+    }
+}
