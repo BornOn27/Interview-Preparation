5a63338ef1307da1449660f062c065cbd5d8ce29
Manoj Kumar Singh <32872442+BornOn27@users.noreply.github.com>
2023-11-07 19:25:52 +0530

    I am getting better everyday

--- src/main/_05_array/_2_medium/_14_SubarraySumEqualsK.java
+++ src/main/_05_array/_2_medium/_14_SubarraySumEqualsK.java
@@ -1,5 +1,9 @@
 package main._05_array._2_medium;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 public class _14_SubarraySumEqualsK {
     //Question-Link ::
 
@@ -8,6 +12,25 @@ public class _14_SubarraySumEqualsK {
     }
 
     public int subarraySum(int[] nums, int k) {
-        return 1;
+        long prefixSum = 0;
+        HashMap<Long, Integer> foundSums = new HashMap<>();
+        foundSums.put(0L, 1);
+
+        int result = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            prefixSum += nums[i];
+//            System.out.println("PrefixSum :: "+prefixSum);
+//            System.out.println(foundSums);
+
+            if(foundSums.containsKey(prefixSum - k)){
+                result += foundSums.get(prefixSum - k);
+            }
+
+            foundSums.put(prefixSum, foundSums.getOrDefault(prefixSum, 0) + 1);
+        }
+
+
+        return result;
     }
 }
